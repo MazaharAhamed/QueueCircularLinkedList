0c58f2ba48b17efa247db45cd21475cad03237ca
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-12 21:32:32 +0530

    Add project files.

---
+++ QueueCircularLinkedList/Demo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace QueueCircularLinkedList
+{
+    class Demo
+    {
+        static void Main(String[] args)
+        {
+            int choice, x;
+
+            QueueCircularL quCL = new QueueCircularL();
+
+            while(true)
+            {
+                Console.WriteLine("1.Queue Size");
+                Console.WriteLine("2.Insert in Queue");
+                Console.WriteLine("3.Delete from Queue");
+                Console.WriteLine("4.Front Element in Queue");
+                Console.WriteLine("5.Display all elements in Queue");
+                Console.WriteLine("6.Quit");
+                Console.WriteLine("Enter Your Choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 6)
+                    break;
+
+                switch(choice)
+                {
+                    case 1:
+                        quCL.Size();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter an element to insert in Queue: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        quCL.Insert(x);
+                        break;
+                    case 3:
+                        Console.WriteLine("Deleted Element is: "+quCL.Delete());
+                        break;
+                    case 4:
+                        Console.WriteLine("Front Element in Queue is: " + quCL.Peek());
+                        break;
+                    case 5:
+                        quCL.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ QueueCircularLinkedList/QueueCircularL.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace QueueCircularLinkedList
+{
+    class QueueCircularL
+    {
+        private Node rear;
+
+        public QueueCircularL()
+        {
+            rear = null;
+        }
+
+        public void Size()
+        {
+            int s = 0;
+            Node p = rear.link;
+            do
+            {
+                s++;
+                p = p.link;
+            } while (p != rear.link);
+            Console.WriteLine("Size of the Queue is: " + s);
+        }
+
+        public bool isEmpty()
+        {
+            return (rear == null);
+        }
+
+        public void Insert(int x)
+        {
+            Node temp = new Node(x);
+            if (rear == null)
+            {
+                rear = temp;
+                rear.link = rear;
+            }
+            else
+            {
+                temp.link = rear.link;
+                rear.link = temp;
+                rear = temp;
+            }
+        }
+
+        public int Delete()
+        {
+            Node temp;
+            if (isEmpty())
+                throw new System.InvalidOperationException("Queue Underflow");
+            temp = rear.link;
+            rear.link = temp.link;
+            return temp.info;
+        }
+
+        public int Peek()
+        {
+            Node temp;
+            if(isEmpty())
+                throw new System.InvalidOperationException("Queue Underflow");
+            temp = rear.link;
+            return temp.info;
+        }
+
+        public void Display()
+        {
+            Node p = rear.link;
+            if (isEmpty())
+                Console.WriteLine("Queue is Empty");
+            Console.WriteLine("Queue is:");
+            do
+            {
+                Console.WriteLine(p.info + " ");
+                p = p.link;
+            } while (p != rear.link);
+            Console.WriteLine("");
+        }
+    }
+}
